08c883c6c9ef8be74517fc7f79770f6c52c1a2c0
GauravInBytes <gode429@gmail.com>
2025-10-04 16:20:14 +0530

    Add project files.

---
+++ Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFramework.Entities
+{
+
+    /// <summary>
+    /// Domain Model for Country
+    /// </summary>
+    public class Country
+    {
+        [Key]
+        public Guid CountryID { get; set; }
+
+        public string? CountryName { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+using EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PersonsDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
